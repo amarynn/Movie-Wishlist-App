ca5c0f5411ae0734405e73119a52bb282cc72837
leo <lmach106@outlook.com>
2023-06-01 14:31:45 +0930

    added fix for empty database response

--- models/user.js
+++ models/user.js
@@ -23,14 +23,14 @@ const User = {
             .then(dbRes => dbRes.rows[0])
     },
 
-    create: (name,email,passwordDigest) => {
+    create: (name, email, passwordDigest) => {
         const sql = `
             INSERT INTO users(name, email, password_digest)
             VALUES ($1, $2, $3)
             RETURNING *
         `
         return db
-        .query(sql, [name, email,   passwordDigest])
+        .query(sql, [name, email, passwordDigest])
         .then(dbRes => dbRes.rows[0].email)
     }
 }
