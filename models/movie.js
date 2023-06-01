6e30b4341f55336acaa2d26c4721370db3112447
AntalThomas <Blake-Heward@hotmail.com>
2023-06-01 10:26:31 +0930

    create movies sql function

--- models/movie.js
+++ models/movie.js
@@ -7,5 +7,16 @@ const Movie = {
         return db
             .query(sql)
             .then(dbRes => dbRes.rows)
+    },
+    create: (title, description, imgLink) => {
+        const sql = `
+            INSERT INTO movies(title, description, img_link)
+            VALUES($1, $2, $3)
+            RETURNING *
+        `
+
+        return db
+            .query(sql, [title, description, imgLink])
+            .then(dbRes => dbRes.rows[0])
     }
 }
