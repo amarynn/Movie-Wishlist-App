ef376ce6e7b5c92070a830454328e84b7997756c
AntalThomas <Blake-Heward@hotmail.com>
2023-06-06 09:36:28 +0930

    Cleaned up the bubbles code

--- client/js/components/edit_movie.js
+++ client/js/components/edit_movie.js
@@ -21,31 +21,7 @@ function renderEditMovie(movie){
                 <button class="btn">Edit Movie</button>
             </form>
             <div class="box">
-                <div class="bubbles">
-                    <span style="--i.11;"></span>
-                    <span style="--i.14;"></span>
-                    <span style="--i.25;"></span>
-                    <span style="--i.16;"></span>
-                    <span style="--i.14;"></span>
-                    <span style="--i.25;"></span>
-                    <span style="--i.12;"></span>
-                    <span style="--i.24;"></span>
-                    <span style="--i.13;"></span>
-                    <span style="--i.11;"></span>
-                    <span style="--i.10;"></span>
-                    <span style="--i.15;"></span>
-                    <span style="--i.17;"></span>
-                    <span style="--i.18;"></span>
-                    <span style="--i.12;"></span>
-                    <span style="--i.24;"></span>
-                    <span style="--i.13;"></span>
-                    <span style="--i.11;"></span>
-                    <span style="--i.10;"></span>
-                    <span style="--i.15;"></span>
-                    <span style="--i.17;"></span>
-                    <span style="--i.18;"></span>
-                    <span style="--i.12;"></span>
-                </div>
+                <div class="bubbles">${renderBubbles()}</div>
             </div>
         </section>
     `
--- client/js/components/login.js
+++ client/js/components/login.js
@@ -17,31 +17,7 @@ function renderLogin() {
                 <button class="btn">Log in</button>
             </form>
             <div class="box">
-                <div class="bubbles">
-                    <span style="--i.11;"></span>
-                    <span style="--i.14;"></span>
-                    <span style="--i.25;"></span>
-                    <span style="--i.16;"></span>
-                    <span style="--i.14;"></span>
-                    <span style="--i.25;"></span>
-                    <span style="--i.12;"></span>
-                    <span style="--i.24;"></span>
-                    <span style="--i.13;"></span>
-                    <span style="--i.11;"></span>
-                    <span style="--i.10;"></span>
-                    <span style="--i.15;"></span>
-                    <span style="--i.17;"></span>
-                    <span style="--i.18;"></span>
-                    <span style="--i.12;"></span>
-                    <span style="--i.24;"></span>
-                    <span style="--i.13;"></span>
-                    <span style="--i.11;"></span>
-                    <span style="--i.10;"></span>
-                    <span style="--i.15;"></span>
-                    <span style="--i.17;"></span>
-                    <span style="--i.18;"></span>
-                    <span style="--i.12;"></span>
-                </div>
+                <div class="bubbles">${renderBubbles()}</div>
             </div>
         </section>
     `
